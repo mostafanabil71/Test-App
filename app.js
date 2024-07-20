1743d6365f70852ba9ba226879e4a50beddf5a21
mostafanabil71 <mostafa.nabile.71@gmail.com>
2024-07-21 02:12:35 +0300

    update jenkinsfilet

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const host = 'localhost';
-const port = 8006;
+const port = 8008;
 
 
 app.use(express.static(path.join(__dirname,'public')));
