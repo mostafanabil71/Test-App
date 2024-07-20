f30b02be66a0a7976aa6e840586d2e2afef1a305
mostafanabil71 <mostafa.nabile.71@gmail.com>
2024-07-20 15:36:48 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const host = 'localhost';
+const port = 8006;
+
+
+app.use(express.static(path.join(__dirname,'public')));
+
+// Route for serving hello.html
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'hello.html'))
+});
+
+// start server
+  app.listen(port, function (error) {
+   if(!error)
+      console.log(`Server is Successfully Running,and App is listening on http://${host}:${port}/`)
+  else 
+      console.log("Error occurred, server can't start", error);
+});
